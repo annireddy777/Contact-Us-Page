122a1102e441864fc4b48f32a4a15b1c4298097a
Anjan Kumar Reddy <54322266+annireddy777@users.noreply.github.com>
2024-09-25 13:35:34 +0530

    Update navigation.jsx

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "/Web Development/React/react-app/src/assests/Frame 2 1.png"
+import logo from "../../assests/Frame 2 1.png"
 import styles from "./Navigation.module.css"
 
 export const Navigation = () => {
